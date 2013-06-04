bmerry/clogs

commit 87be3769edeb9309c1ad20f49b2d7c887e7e6233

Fix clogs-benchmark with 3-element padded value types (fixes: #20)

tools/clogs-benchmark.cpp
@@ -206,7 +206,7 @@ static cl::Buffer randomBuffer(
     std::size_t elements, const clogs::Type &type,
     cl_ulong minValue, cl_ulong maxValue)
 {
-    int length = type.getLength();
+    int length = type.getSize() / clogs::Type(type.getBaseType()).getSize();
     switch (type.getBaseType())
     {
     case clogs::TYPE_UCHAR:
@@ -261,7 +261,7 @@ static cl::Buffer randomBuffer(
     RANDOM_NAMESPACE::mt19937 &engine,
     std::size_t elements, const clogs::Type &type)
 {
-    int length = type.getLength();
+    int length = type.getSize() / clogs::Type(type.getBaseType()).getSize();
     switch (type.getBaseType())
     {
     case clogs::TYPE_UCHAR:

commit 8238346b118f927a8c92637cc97976941f28f265

Fix broken support for --value-type=void in clogs-benchmark
This was broken by 87be3769edeb9309c1ad20f49b2d7c887e7e6233

tools/clogs-benchmark.cpp
@@ -75,7 +75,7 @@ static clogs::Type matchType(const std::string &typeName)
                     return type;
             }
     }
-    std::cerr << "Type '" << typeName << " is not recognized.\n";
+    std::cerr << "Type '" << typeName << "' is not recognized.\n";
     std::exit(1);
 }
 
@@ -206,6 +206,9 @@ static cl::Buffer randomBuffer(
     std::size_t elements, const clogs::Type &type,
     cl_ulong minValue, cl_ulong maxValue)
 {
+    if (type.getBaseType() == clogs::TYPE_VOID)
+        return cl::Buffer();
+
     int length = type.getSize() / clogs::Type(type.getBaseType()).getSize();
     switch (type.getBaseType())
     {
@@ -250,7 +253,7 @@ static cl::Buffer randomBuffer(
             return randomBuffer<T>(queue, engine, elements, length, (T) minValue, (T) maxValue);
         }
     default:
-        return cl::Buffer(); // handles void
+        return cl::Buffer(); // should never be reached
     }
 }
 
@@ -261,6 +264,9 @@ static cl::Buffer randomBuffer(
     RANDOM_NAMESPACE::mt19937 &engine,
     std::size_t elements, const clogs::Type &type)
 {
+    if (type.getBaseType() == clogs::TYPE_VOID)
+        return cl::Buffer();
+
     int length = type.getSize() / clogs::Type(type.getBaseType()).getSize();
     switch (type.getBaseType())
     {
@@ -322,7 +328,7 @@ static cl::Buffer randomBuffer(
             return randomBuffer<T>(queue, engine, elements, length);
         }
     default:
-        return cl::Buffer(); // handles void
+        return cl::Buffer(); // should never be reached
     }
 }
 